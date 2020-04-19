de154768f688a7f3fcee051518713d547374bda3
GusFurtado <62435505+GusFurtado@users.noreply.github.com>
2020-04-19 20:12:26 -0300

    Adicionar módulo Favoritos
    Primeira versão do módulo Favoritos.
    Este é um módulo para fácil acesso a dados selecionados.
    Atualmente inclui função para consulta de moedas, taxas cambiais, IPC-A e o catálogo de bancos de dados abertos.
    Este é um módulo de menor prioridade, então novas funções serão adicionadas sem frequência definida.

---
+++ favoritos.py
@@ -0,0 +1,48 @@
+# Pacote para fácil acesso a dados selecionados
+# Autor: Gustavo Furtado da Silva
+
+
+
+import pandas as pd
+from datetime import datetime
+
+# Principais moedas internacionais
+def moedas():
+    query = r"https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/Moedas?$top=100&$format=json"
+    return pd.DataFrame(pd.read_json(query)['value'].to_list())
+
+# Taxa de câmbio das principais moedas internacionais
+def cambio(moedas='USD', data_inicial='01-01-2000', data_final='01-01-2020'):
+
+    if isinstance(moedas, str):
+        query = f"https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda='{moedas}'&@dataInicial='{data_inicial}'&@dataFinalCotacao='{data_final}'&$top=10000&$filter=contains(tipoBoletim%2C'Fechamento')&$format=json&$select=cotacaoVenda,dataHoraCotacao"
+        cotacoes = pd.DataFrame(pd.read_json(query)['value'].to_list())
+    else:
+        try:    
+            cotacao_moedas = []
+            for moeda in moedas:
+                query = f"https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda='{moeda}'&@dataInicial='{data_inicial}'&@dataFinalCotacao='{data_final}'&$top=10000&$filter=contains(tipoBoletim%2C'Fechamento')&$format=json&$select=cotacaoVenda,dataHoraCotacao"
+                cotacao_moeda = pd.DataFrame(pd.read_json(query)['value'].to_list())
+                cotacao_moeda.dataHoraCotacao = cotacao_moeda.dataHoraCotacao.apply(lambda x: datetime.strptime(x[:10], '%Y-%m-%d'))
+                cotacao_moedas.append(cotacao_moeda.rename(columns={'cotacaoVenda': moeda}).set_index('dataHoraCotacao'))
+
+            cotacoes = pd.concat(cotacao_moedas, axis=1)
+
+        except:
+            raise TypeError("O campo 'moedas' deve ser o código de três letras maiúsculas da moeda ou um objeto iterável de códigos.")
+    
+    return cotacoes
+
+# Valor mensal do índice IPC-A
+def ipca():
+
+    ipca_query = r'https://api.bcb.gov.br/dados/serie/bcdata.sgs.4448/dados?formato=json'
+    ipca = pd.read_json(ipca_query)
+    ipca.data = pd.to_datetime(ipca.data)
+    ipca = ipca.set_index('data').rename(columns={'valor':'IPCA Mensal'})
+    
+    return ipca
+
+# Catalogo de iniciativas oficiais de dados abertos no Brasil
+def catalogo():
+    return pd.read_csv('https://raw.githubusercontent.com/dadosgovbr/catalogos-dados-brasil/master/dados/catalogos.csv')
