1e53efb0745f3d2f8c97c866d5d22a854c16d388
GusFurtado <gustavofurtado2@gmail.com>
2024-03-14 12:31:19 -0300

    Automatizar extração de versão em `setup.py`

--- setup.py
+++ setup.py
@@ -1,9 +1,39 @@
 from setuptools import setup
 from os import path
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
+
+def get_long_description():
+    """
+    Retrieve the long description from the README.md file.
+
+    Returns
+    -------
+    str
+        The long description read from the README.md file.
+    """
+
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, "README.md"), encoding="utf-8") as file:
+        return file.read()
+
+
+def get_version():
+    """
+    Retrieve the version number from the package __init__.py file.
+
+    Returns
+    -------
+    str
+        The version number extracted from the __init__.py file.
+    """
+
+    with open("DadosAbertosBrasil/__init__.py") as file:
+        lines = file.readlines()
+        for line in lines:
+            if line.startswith("__version__"):
+                return line.split('"')[1]
+        raise RuntimeError("Versão não encontrada.")
+
 
 setup(
     name="DadosAbertosBrasil",
@@ -12,10 +42,10 @@ setup(
         "DadosAbertosBrasil._utils",
         "DadosAbertosBrasil._ibge",
     ],
-    version="1.0.0",
+    version=get_version(),
     license="MIT",
     description="Pacote Python para acesso a dados abertos e APIs do governo brasileiro.",
-    long_description=long_description,
+    long_description=get_long_description(),
     long_description_content_type="text/markdown",
     author="Gustavo Furtado",
     author_email="gustavofurtado2@gmail.com",
