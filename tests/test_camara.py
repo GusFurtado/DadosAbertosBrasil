7b9d65cee8b1c2aca93ed43b9f174e583bb4ab0a
GusFurtado <gustavofurtado2@gmail.com>
2024-12-17 20:00:17 -0300

    ✅ Aumentar cobertura de teste de `camara`

--- tests/test_camara.py
+++ tests/test_camara.py
@@ -36,6 +36,61 @@ def test_lista_partidos():
     assert not df.empty
 
 
+def test_lista_proposicoes():
+    df = camara.lista_proposicoes()
+    assert not df.empty
+
+
 def test_lista_votacoes():
     df = camara.lista_votacoes(itens=5, asc=True)
     assert not df.empty
+
+
+def test_referencias():
+    df = camara.referencias("ufs")
+    return not df.empty
+
+
+def test_Bloco():
+    obj = camara.Bloco(586)
+    assert obj.dados
+
+
+def test_Deputado():
+    obj = camara.Deputado(220593)
+    assert obj.dados
+
+
+def test_Evento():
+    obj = camara.Evento(74519)
+    assert obj.dados
+
+
+def test_Frente():
+    obj = camara.Frente(55660)
+    assert obj.dados
+
+
+def test_Legislatura():
+    obj = camara.Legislatura(56)
+    assert obj.dados
+
+
+def test_Orgao():
+    obj = camara.Orgao(4)
+    assert obj.dados
+
+
+def test_Partido():
+    obj = camara.Partido(13)
+    assert obj.dados
+
+
+def test_Proposicao():
+    obj = camara.Proposicao(15151)
+    assert obj.dados
+
+
+def test_Votacao():
+    obj = camara.Votacao("2430143-148")
+    assert obj.dados
